f00bf6a9a41d3e65a5f5d91e9d1a1549bd196cb6
Claudinei Perboni <cperbony@gmail.com>
2016-10-01 23:32:08 -0300

    Atualização dos VO´s

---
+++ src/java/com/bakeryfactory/cadastros/java/ProdutoSubgrupoVO.java
@@ -0,0 +1,141 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Claudinei Aparecido Perboni • contact: cperbony@gmail.com.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.bakeryfactory.cadastros.java;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.openswing.swing.message.receive.java.ValueObjectImpl;
+
+/**
+ *
+ * @author Claudinei Aparecido Perboni • contact: cperbony@gmail.com
+ */
+@Entity
+@Table(name = "produto_subgrupo")
+@NamedQueries({
+    @NamedQuery(name = "ProdutoSubgrupoVO.findAll", query = "SELECT p FROM ProdutoSubgrupoVO p")})
+public class ProdutoSubgrupoVO extends ValueObjectImpl implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "descricao")
+    private String descricao;
+    @JoinColumn(name = "classe_produto_grupo_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private ClasseProdutoGrupoVO classeProdutoGrupoId;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "produtoSubgrupoId")
+    private List<ProdutoVO> produtoVOList;
+
+    public ProdutoSubgrupoVO() {
+    }
+
+    public ProdutoSubgrupoVO(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public ClasseProdutoGrupoVO getClasseProdutoGrupoId() {
+        return classeProdutoGrupoId;
+    }
+
+    public void setClasseProdutoGrupoId(ClasseProdutoGrupoVO classeProdutoGrupoId) {
+        this.classeProdutoGrupoId = classeProdutoGrupoId;
+    }
+
+    public List<ProdutoVO> getProdutoVOList() {
+        return produtoVOList;
+    }
+
+    public void setProdutoVOList(List<ProdutoVO> produtoVOList) {
+        this.produtoVOList = produtoVOList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ProdutoSubgrupoVO)) {
+            return false;
+        }
+        ProdutoSubgrupoVO other = (ProdutoSubgrupoVO) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.bakeryfactory.cadastros.java.ProdutoSubgrupoVO[ id=" + id + " ]";
+    }
+    
+}
